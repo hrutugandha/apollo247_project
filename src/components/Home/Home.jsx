1e7536ca751e20718a4e3774122c94956f507ebf
nHari9 <nharikishorereddy9@gmail.com>
2022-05-06 11:39:03 +0530

    home static

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,135 @@
 import React from 'react'
-import "./Home"
+import "../styles/Home.css"
 import Nav from './Navbar';
 
 export default function Home() {
-  return (
-    <div className='body'>
-        
+  return (<>
+   <div className='body'>
+   <Nav/>
+        <div className='content'>
+          <div className='one'>
+
+            <div className='onecontent'>
+
+              <div className='oneimg'>
+              <img class="img1" src="https://newassets.apollo247.com/images/img-doctors@1x.jpg" alt="Online Doctor Consultation &amp; Medicines"/>
+              </div>
+
+              <div className='leftone'>
+              <span class="lspan1">hello there!</span>
+              <p> Always caring about your health, we are here to help you!</p>
+              <div className='ldiv'>
+                <div className='ldivs'>
+                  <a href="">
+                    <div className='ldivimg'>
+                    <img alt="" src="https://newassets.apollo247.com/images/ic-doctor.svg"/>
+                    </div>
+
+                    <div className='ldivh'>
+                      <h1>Book Appointment with Apollo Doctors</h1>
+                    </div>
+                    <img src="https://newassets.apollo247.com/images/ic_arrow_right_white.svg" alt="" className='limg'></img>
+                  </a>
+                </div>
+
+                <div className='ldivs'>
+                  <a href="">
+                    <div className='ldivimg'>
+                    <img alt="" src="https://newassets.apollo247.com/images/ic_medicines.svg"/>
+                    </div>
+
+                    <div className='ldivh'>
+                      <h1>Buy Medicines and Essentials</h1>
+                    </div>
+                    <img src="https://newassets.apollo247.com/images/ic_arrow_right_white.svg" alt="" className='limg'></img>
+                  </a>
+                </div>
+
+                <div className='ldivs'>
+                  <a href="">
+                    <div className='ldivimg'>
+                    <img alt="" src="https://newassets.apollo247.com/images/ic_home_test.svg"></img>
+                    </div>
+
+                    <div className='ldivh'>
+                      <h1>Book Lab Tests</h1>
+                    </div>
+                    <img src="https://newassets.apollo247.com/images/ic_arrow_right_white.svg" alt="" className='limg'></img>
+                  </a>
+                </div>
+
+                <div className='ldivs'>
+                  <a href="">
+                    <div className='ldivimg'>
+                    <img alt="" src="https://newassets.apollo247.com/images/ic-prescription.svg"/>
+                    </div>
+
+                    <div className='ldivh'>
+                      <h1>View Health Records</h1>
+                    </div>
+                    <img src="https://newassets.apollo247.com/images/ic_arrow_right_white.svg" alt="" className='limg'></img>
+                  </a>
+                </div>
+
+              </div>
+              </div>
+          
+          </div>
+
+          <div className='two'>
+            
+          <div className='pic'>
+          <a href="/apollo-prohealth" className='apic'>
+            <img src="https://newassets.apollo247.com/images/prohealth/prohealthSliderTwo.jpg"/>
+
+            </a>
+          </div>
+
+            <div className='tdiv1'>
+              <div className='tdiv'>
+                <div className='tdivs'>
+                  <div className='tdivh'>
+                    <div className='tdivn'>
+                    For COVID-19 Vaccination related queries
+                    </div>
+                    <div className='options'>
+                      <div className='btns'>
+                        <a href="" className='abtns'>
+                          <span></span>
+                          <span>FAQ's & Articles</span>
+                        </a>
+                      </div>
+                      <div className='btns'>
+                        <a href="" className='abtns'>
+                          <span></span>
+                          <span>Vaccination Queries</span>
+                        </a>
+                      </div>
+                      <div className='btns'>
+                        <a href="" className='abtns'>
+                          <span></span>
+                          <span>Chat with us</span>
+                        </a>
+                      </div>
+                      <div className='btns'>
+                        <a href="" className='abtns'>
+                          <span></span>
+                          <span>Cowin Registration</span>
+                        </a>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+
+          </div>
+        </div>
       
     </div>
-  )
+  </>)
 }
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import  "./Navbar.css"
-import { Link } from './links';
 
 const Nav=()=>{
-    var links=["home","explore","about"]
-    return(
-        <>
+   return(<>
         <nav>
-            <div className='logo'>
-            <img src="https://newassets.apollo247.com/images/ic_logo.png" title="Online Doctor Consultation &amp; Medicines" alt="Online Doctor Consultation &amp; Medicines"/>
-            </div>
-            <div className='links'>
-                {
-                    links.map((e,i)=><Link text={e} key={i}/>)
-                }
-            </div>
-            <div className='button'>
-                <button>Contact</button>
+            <div className='navf'>
+                <header>
+                    
+                </header>
             </div>
         </nav>
-        </>
-    )
+        
+        </>)
 }
 
 export default Nav;
