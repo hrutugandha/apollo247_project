3bda39c1f1595a9165b7f5f82c7ee9dbf12e168e
nHari9 <nharikishorereddy9@gmail.com>
2022-05-08 10:27:47 +0530

    home

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -6,7 +6,17 @@ const Nav=()=>{
         <nav>
             <div className='navf'>
                 <header>
-                    
+                    <div>
+                        <a href="" className='logoa'>
+                        <img src="https://newassets.apollo247.com/images/ic_logo.png" title="Online Doctor Consultation &amp; Medicines" alt="Online Doctor Consultation &amp; Medicines"/>
+                        </a>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </header>
             </div>
         </nav>
